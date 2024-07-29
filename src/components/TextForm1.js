52cc01e389dc4d98c2c857c7d68eb1d15b1934d2
Nimisha Rawat <nimisha17rawat@gmail.com>
2024-07-29 13:18:52 +0530

    code updates

--- src/components/TextForm1.js
+++ src/components/TextForm1.js
@@ -1,41 +1,66 @@
-import React ,{useState}from 'react'
+import React, { useState } from "react";
 
 export default function TextForm1(props) {
-  const handelClearClick = ()=>{
-    let newText = '';
+  const handelClearClick = () => {
+    let newText = "";
     setText(newText);
+  };
+  const handelCopy = () =>{
+    var text = document.getElementById('myBox');
+    text.select();
+    navigator.clipboard.writeText(text.value);
   }
-    const handelLowClick = () =>{
-        let newText = text.toLowerCase();
-        setText(newText);
-    }
-    const handelUpClick = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-    }
+  const handelExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+  const handelLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handelUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-    const handelOnChange = (event) => {
-        console.log("Now you can write new text with the existing one");
-        setText(event.target.value);
-    }
-    const[text,setText] = useState("Enter text here");
-    //setText("newText");
+  const handelOnChange = (event) => {
+    console.log("Now you can write new text with the existing one");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here");
+  //setText("newText");
   return (
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" id="myBox" value={text} onChange={handelOnChange}rows="8" ></textarea>
+        <textarea
+          className="form-control"
+          id="myBox"
+          value={text}
+          onChange={handelOnChange}
+          rows="8"
+        ></textarea>
       </div>
-      <div >
-      <button className='btn btn-primary mx-1' onClick={handelUpClick}>Convert to UpperCase</button>
-      <button className='btn btn-primary mx-1' onClick={handelLowClick}>Convert to LowerCase</button>
-      <button className='btn btn-primary mx-1' onClick={handelClearClick}>Clear</button>
-      </div>
-      <p>Word Count {text.split(" ").length} characters {text.length}</p>
-      <h2>Preview</h2>
       <div>
-        {text}
+        <button className="btn btn-primary mx-1" onClick={handelUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handelLowClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-1"onClick={handelExtraSpace}>Remove extra space</button>
+        <button className="btn btn-primary mx-1" onClick={handelCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handelClearClick}>
+          Clear
+        </button>
       </div>
+      <p>
+        Word Count {text.split(" ").length} characters {text.length}
+      </p>
+      <h2>Preview</h2>
+      <div>{text}</div>
     </div>
-  )
+  );
 }
